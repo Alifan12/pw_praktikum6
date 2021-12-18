cc6203b34146604df1567c740edb9f2d354a0f30
Alifan12 <alif129310@gmail.com>
2021-12-18 23:50:43 +0700

    first commit

---
+++ praktikum6.js
@@ -0,0 +1,9 @@
+let kategori = document.getElementById("kategori");
+let arrKategori = [ {Kategori : 'Sosial', Abodemen: 10000, TarifPerKWH: 300, pajak: 0},
+                    {Kategori : 'Rumah', Abodemen: 30000, TarifPerKWH: 500, pajak: 0.10},
+                    {Kategori : 'Apartemen', Abodemen: 50000, TarifPerKWH: 750, pajak: 0.15},
+                    {Kategori : 'Industri', Abodemen: 75000, TarifPerKWH: 1000, pajak: 0.20},
+                    {Kategori : 'Villa', Abodemen: 100000, TarifPerKWH: 1250, pajak: 0.25}
+                  ];
+
+console.log(arrKategori);
